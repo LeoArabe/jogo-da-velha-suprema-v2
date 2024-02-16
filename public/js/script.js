c2d32e6f94b49830afda996418f1a23a35bd4ca2
LeoArabe <103582132+LeoArabe@users.noreply.github.com>
2024-02-16 08:57:09 -0300

    test

--- public/js/script.js
+++ public/js/script.js
@@ -33,7 +33,7 @@ const celle0 = [[0, 1, 2, 3, 4, 5, 6, 7, 8]
 ];
 
 export let currentPlayerSymbol;
-let circleTurn = false;
+export let circleTurn = false;
 let currentPlayerName;
 let currentRoomId;
 let turn;
