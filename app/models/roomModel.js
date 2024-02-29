4996543063b128e341e96948ba3f488e7fe418c0
LeoArabe <103582132+LeoArabe@users.noreply.github.com>
2024-02-29 02:15:05 -0300

    fix server

--- app/models/roomModel.js
+++ app/models/roomModel.js
@@ -8,6 +8,6 @@ exports.createRoom = (roomId, player) => {
 exports.addPlayerToRoom = (roomId, player) => {
     rooms[roomId].players.push(player); // Adiciona o jogador à lista de jogadores da sala
 };
-exports.addMoveToRoom = (movePlayer) => {
-    rooms[id].moves.push(movePlayer);
+exports.addMoveToRoom = (roomId, position) => {
+    rooms[roomId].moves.push(position);
 }
